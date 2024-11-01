fd02cdf2d01083eab7ccca5e7aa417df5f12ec87
Odir Albuquerque <55720144+Zenophile@users.noreply.github.com>
2024-11-01 14:35:21 -0300

    Primeiro passo

---
+++ BIkefacil.py
@@ -0,0 +1,12 @@
+
+while True:
+    print("################################\n################################"
+          "\n**Bem vindo ao App BIKEFACIL!***\n"
+          "Digite 1 para login\n"
+          "Digite 2 para cadastrar usuário\n"
+          "Digite 3 para sair\n"
+          "################################\n################################")
+    escolha = int(input())
+    if escolha == 3:
+
+        break
